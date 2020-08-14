e05f7658c3f0e432c787b9a96a557e0ca05ae8bf
Adams <avadams@chsomaha.org>
2020-08-14 15:12:31 -0500

    2

---
+++ puzzleMonkeyHeader.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Puzzle Monkey
+// @namespace    http://puzzleMonkey.aveadams.com/
+// @version      0.27
+// @description  Tools to make epuzzle.info more functional
+// @author       Avery Adams
+// @match        http*://*.epuzzle.info*
+// @connect      epuzzle.info/
+// @grant        none
+// @require      https://raw.githubusercontent.com/AveryAdams/Epuzzle/master/puzzleMonkeyCode.js
+// ==/UserScript==
